7c91a74df74d0769f5871706fadcf352201b6a86
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-23 23:39:09 +0700

    Use lmer With (In) R Software

--- lmer/lmer.R
+++ lmer/lmer.R
@@ -1,7 +1,7 @@
 # Olah Data Semarang
 # WhatsApp : +6285227746673
 # IG : @olahdatasemarang_
-# Fit Linear Mixed Effects Models Use lmer Or With (In) R Software
+# Fit Linear Mixed Effects Models Use lmer With (In) R Software
 # Fitting Linear Mixed-Effects Models Use lmer With (In) R Software
 install.packages("Matrix")
 install.packages("lme4")
@@ -9,11 +9,11 @@ install.packages("tm")
 library("tm")
 library("lme4")
 covRob = read.csv("https://raw.githubusercontent.com/timbulwidodostp/lmer/main/lmer/lmer.csv",sep = ";")
-# Estimate Fit Linear Mixed Effects Models Use lmer Or With (In) R Software
+# Estimate Fit Linear Mixed Effects Models Use lmer With (In) R Software
 lmer<-as.data.frame(as.matrix(lmer))
 lmer = lmer(Denpenden ~ Indenpenden_1 + (1|Indenpenden_2), data = lmer)
 summary(lmer)
-# Fit Linear Mixed Effects Models Use lmer Or With (In) R Software
+# Fit Linear Mixed Effects Models Use lmer With (In) R Software
 # Fitting Linear Mixed-Effects Models Use lmer With (In) R Software
 # Olah Data Semarang
 # WhatsApp : +6285227746673
